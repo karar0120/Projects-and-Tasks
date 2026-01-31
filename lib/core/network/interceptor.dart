80b21ee0a9fcd1271a3c69904199dabfc4e3cf5e
karar2402-ai <ahmed.muhammed@paytabs.com>
2026-01-31 21:56:12 +0200

    fixed all issues

--- lib/core/network/interceptor.dart
+++ lib/core/network/interceptor.dart
@@ -3,10 +3,8 @@ import 'package:provider/provider.dart';
 import 'package:projectsandtasks/core/constants/cache_consts.dart';
 import 'package:projectsandtasks/core/utils/UserDataController.dart';
 import 'package:projectsandtasks/main.dart';
-import 'package:projectsandtasks/shared/models/environment.dart';
 import 'package:projectsandtasks/shared/services/localeServices/locale_services.dart';
 
-import 'dio_consumer.dart';
 
 class AppInterceptors extends Interceptor {
   @override
@@ -33,29 +31,7 @@ class Validate {
       String? oldToken = LocaleServices.getString(key: CacheConsts.accessToken);
       String? oldRefreshToken = LocaleServices.getString(key: CacheConsts.refreshToken);
       if (oldToken != null && oldRefreshToken != null) {
-        final response = await WebService.getRefershToken(
-          controller: Environment.abpUrl,
-          endpoint: "Authenticate/refresh-token-v2",
-          query: {"api-version": '1.0'},
-          body: {"token": oldToken, "refreshToken": oldRefreshToken},
-        );
 
-        return response.fold(
-          (error) async {
-            userDataController.logout(MyApp.navigatorKey.currentContext!);
-          },
-          (result) async {
-            await LocaleServices.setData(
-              key: CacheConsts.accessToken,
-              value: '${result.data['payload']['token']}',
-            );
-            await LocaleServices.setData(
-              key: CacheConsts.refreshToken,
-              value: '${result.data['payload']['refreshToken']}',
-            );
-            await LocaleServices.cacheLoginResponseBase64(result.data['payload']);
-          },
-        );
       } else {
         userDataController.logout(MyApp.navigatorKey.currentContext!);
       }
